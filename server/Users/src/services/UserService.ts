2915925d196ff16cc4a6bbd95bbc61319b919981
JuanManuelBR <162181239+JuanManuelBR@users.noreply.github.com>
2025-11-09 00:13:28 -0500

    Delete User
    Service & controller for DeleteUser.

--- server/Users/src/services/UserService.ts
+++ server/Users/src/services/UserService.ts
@@ -33,8 +33,8 @@ export class UserService {
         throw new Error("El correo electrónico ya está en uso");
       }
 
-      const salt = 10;
-      const hashed_password = await bcrypt.hash(data.contrasena, salt);
+
+      const hashed_password = await bcrypt.hash(data.contrasena, 10);
       data.contrasena = hashed_password;
 
       const user = this.user_repository.create(data);
@@ -46,6 +46,24 @@ export class UserService {
       console.error("No se pudo crear el nuevo usuario", error.message);
 
       throw error;
+    }
+  }
+
+  async deleteUser(id: number) {
+    try {
+      const usuario_buscar = await this.user_repository.findOne({
+        where: { id },
+      });
+
+      if (!usuario_buscar) {
+        throw new Error(`No se encontró el usuario con el id ${id}`);
+      }
+
+      await this.user_repository.remove(usuario_buscar);
+
+      return `Se eliminó al usuario correctamente`;
+    } catch (error) {
+      throw new Error("No se pudo eliminar el usuario");
     }
   }
 }
