0e7cfd4109ed7c8b0771649e480031cea5a04535
JuanManuelBR <162181239+JuanManuelBR@users.noreply.github.com>
2025-11-17 16:58:36 -0500

    Fix UserRoutes
    Fixed the method for AddUser, it required a previous login to create a new user.

--- server/Users/src/routes/UserRoutes.ts
+++ server/Users/src/routes/UserRoutes.ts
@@ -5,12 +5,13 @@ import { authMiddleware } from "@src/middlewares/auth";
 const router = Router();
 router.post("/login", UserController.login);
 router.post("/logout", UserController.logout);
+router.post("/", UserController.AddUser);
 router.use(authMiddleware);
 router.get("/", UserController.getUsers);
 
 router.get("/:id", UserController.getUserById);
 
-router.post("/", UserController.AddUser);
+
 
 router.delete("/:id", UserController.deleteUser);
 
